ca19e045d02cd4fc49146c7b5dea92308c8cd352
Phosisthedolphin <admin@joeldpeterson.com>
2019-05-03 16:24:38 -0600

    pagination 2

--- lightpagination.php
+++ lightpagination.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
 
         $con = mysqli_connect('localhost', 'root', '');
@@ -32,8 +41,7 @@
 
         while($row = mysqli_fetch_array($result)) {
             // echo $row['item_description'] . '<br>';
-            echo
-            "<div class='catalogue-card'>".
+            echo "<div class='catalogue-card'>".
             "<div class='catalogue-card-description'>".
             $row["item_description"].
             "</div>".
@@ -85,3 +93,5 @@
 
 
     ?>
+</body>
+</html>
