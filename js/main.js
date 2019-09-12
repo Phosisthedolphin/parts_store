671ecd02b663f001a6cc02e237c943f1bb15c7af
Phosisthedolphin <admin@joeldpeterson.com>
2019-09-12 12:13:08 -0600

    JS

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ currentSearchWord = null;
 resultsPerPage = 40;
 currentPage = 0;
 totalResults = null;
+cartData = [];
+arrObj = [];
+cartPartNumber = null;
+cartDescription = null;
 
 function getCategories(sub_category=null, manufacturer=null){
   req_string = "/api/category.php";
@@ -208,7 +212,9 @@ function updateHeader(){
     "LIGHT": "LIGHTING",
     "ELECTRIC": "ELECTRICAL",
     "HOSEBAY": "HOSES",
-    "BOLT": "BOLTS"
+    "BOLT": "BOLTS",
+    "IDENTIFI": "IDENTIFICATION",
+    "BRAKE/SU": "BRAKE/SUSPENSION"
   };
 
   $subCategoryMap = {
@@ -217,19 +223,35 @@ function updateHeader(){
     "REPAIRKI": "REPAIR KIT",
     "EMERGENCY": "EMERGENCY",
     "FIBERGLA": "FIBER GLASS",
+    "CHEM/HYD": "CHEMICAL/HYDRAULIC",
     "INCANDES": "INCANDESCANT",
-    "ALLTHRD": "ALL THREAD"
+    "ALLTHRD": "ALL THREAD",
+    "HYDRAULI": "HYDRAULIC",
+    "SIGHTGLA": "SIGHT GLASS",
+    "STAINLES": "STAINLESS STEEL",
+    "COMPOSIT": "COMPOSITE",
+    "SUSPENSI": "SUSPENSION",
+    "PRESS/VA": "PRESS/VAC",
+    "SUPERSTO": "SUPERSTOP"
+
+    
   };
 
   $manufacturerMap = {
     "ARCTICFL": "ARCTICFLEX",
     "SPEEDDEM": "SPEED DEMON",
     "TRUCKLIT": "TRUCKLITE",
-    "REELCRAFT": "REELCRAFT",
+    "REELCRAF": "REELCRAFT",
     "HENDRICK": "HENDRICKSON",
     "GORMAN R": "GORMAN RUPP",
     "ALLEGHEN": "ALLEGHENY",
-    "PHILLIPS": "PHILIPS"
+    "PHILLIPS": "PHILIPS",
+    "BERTOLIN": "BERTOLINI",
+    "ROCHESTE": "ROCHESTER",
+    "VICTAULI": "VICTAULIC",
+    "COLEHERS": "COLE HERSEE",
+    "HONEYWEL": "HONEY WELL",
+    "CHALLENG": "CHALLENGER"
   };
 
   newList = $("<ul>");
@@ -335,6 +357,204 @@ function updateHeader(){
   }
 }
 
+function updateCart() {
+  let cart = document.getElementById("cart");
+  cart = $("#cart");
+  cart.text("");
+  arrObj.forEach(function(element) {
+    
+
+    cartItem = $("<div/>");
+    cartItem.attr("id", "cartItem");
+    
+    cartPartNumber = $("<div/>");
+    cartPartNumber.attr("class", "cartPartNumber");
+    cartPartNumber.text(element.part);
+
+    cartItemDescription = $("<div/>");
+    cartItemDescription.attr("class", "cartItemDescription");
+    cartItemDescription.text(element.description);
+
+    cartItemQuantity = $("<div/>");
+    cartItemQuantity.attr("class", "cartItemQuantity");
+    cartItemQuantity.text(element.quantity);
+
+    cartButtonUp = $("<button/>");
+    cartButtonUp.attr("id", "cartButtonAdd");
+    cartButtonUp.attr("data", element.part);
+    cartButtonUp.on("click", cartButtonAdd);
+    cartButtonUp.text("+");
+
+    cartButtonDown= $("<button/>");
+    cartButtonDown.attr("id", "cartButtonRemove");
+    cartButtonDown.attr("data", element.part);
+    cartButtonDown.on("click", cartButtonRemove);
+    cartButtonDown.text("-");
+
+    cartItemDelete = $("<button/>");
+    cartItemDelete.attr("id", "cartItemDelete");
+    cartItemDelete.text("Delete");
+
+    cartSubmitButton = $("<button/>");
+    cartSubmitButton.on("click", submitOrderButton);
+    cartSubmitButton.text("Complete Order");
+    
+    cartItem.append(cartPartNumber);
+    cartItem.append(cartItemDescription);
+    cartItem.append(cartItemQuantity);
+    cartItem.append(cartButtonUp);
+    cartItem.append(cartButtonDown);
+    cartItem.append(cartItemDelete);
+    cart.append(cartItem);
+    
+  
+      
+    });
+    cart.append(cartSubmitButton);
+}
+
+$(function() {
+
+	// Get the form.
+	var form = $('#ajax-contact');
+
+	// Get the messages div.
+	var formMessages = $('#form-messages');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+    // Stop the browser from submitting the form.
+    
+    e.preventDefault();
+    // e.stopImmediatePropagation();
+
+    // Serialize the form data.
+    let user_form_data = $(form).serialize();
+    let data_modifier = "&data="
+    let user_part_data = encodeURIComponent(JSON.stringify(arrObj));
+    let all_form_data = user_form_data + data_modifier + user_part_data;
+
+    var formData = all_form_data;
+    
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#name, #email, #message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+      }
+      
+      console.log(data.responseText);
+      console.log(all_form_data);
+      console.log($(form).serialize());
+      console.log($(form));
+      console.log(encodeURIComponent(JSON.stringify(arrObj)))
+		});
+
+  });
+
+});
+
+
+
+
+
+// Submit the form using AJAX.
+
+
+function submitButton(something) {
+  window.scroll(0,0);
+  cartData = ($(this).attr("data").split(','));
+
+let cartObj = {
+  part: cartData[0],
+  description: cartData[1],
+  quantity: 1
+}
+match = false;
+arrObj.forEach(function(cartObject){
+  if (cartObject.part == cartData[0])
+  {
+       match = true;
+       cartObject.quantity++
+  }
+})
+if (!match) arrObj.push(cartObj);
+console.log(arrObj);
+
+  let cart = document.getElementById("cart");
+
+
+  document.getElementById("cart").style.display = "flex";
+
+  cart = $("#cart");
+  cart.text("");
+  updateCart();
+  
+  console.log(arrObj);
+}
+
+function cartButtonAdd(something) {
+  for (var i = 0; i < arrObj.length; i++){
+    if (arrObj[i].part == $(this).attr("data"))
+    {
+         match = true;
+         arrObj[i].quantity++
+    } else if (arrObj[i].part == 0) {
+      arrObj[i].part == 1;
+    }
+    console.log(arrObj);
+  }
+
+  updateCart();
+  
+}
+
+
+function cartButtonRemove(something) {
+  for (var i = 0; i < arrObj.length; i++) {
+    if (arrObj[i].part == $(this).attr("data"))
+    {
+      match = true;
+      arrObj[i].quantity--
+    }
+
+  }
+  updateCart();
+}
+
+function cartButtonDelete() {
+
+}
+
+function submitOrderButton() {
+  let checkout = document.getElementById("checkout")
+
+  document.getElementById("checkout").style.display = "flex";
+}
+
 function updateBody()
 {
   
@@ -404,6 +624,14 @@ function updateBody()
         lilImage.on("click", thumbClick)
         catalogueCardInner.append(lilImage);
       });
+
+      cartButton = $("<div/>");
+      cartButton.attr("class", "cartButton");
+      // cartButton.attr("data", JSON.stringify(element));
+      cartButton.attr("data", element);
+      cartButton.on("click", submitButton )
+      cartButton.text("Add to cart");
+      catalogueCardInner.append(cartButton);
 
       
       catalogueCard.append(catalogueCardInner);
@@ -495,6 +723,7 @@ function pageClick()
   updatePage();
 }
 
+
 $('img.thumb').on("click", thumbClick);
 
 function updatePage()
@@ -510,5 +739,6 @@ $((function(){
   //can use JS to get the things.
   updateHeader();
   updateBodyPagination();
+  cart();
   // updatePage();
 }));
